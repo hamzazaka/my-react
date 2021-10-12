8794dea8d1836693167be99151268ffc4d2746d6
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-12 15:55:07 -0400

    just wtying the blog preview

--- blog-app/src/components/Home.js
+++ blog-app/src/components/Home.js
@@ -12,9 +12,7 @@ export default function Home() {
             {blogs.map((blog)=>{
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                    <h2>i am ahamza</h2>
-         
+                    <p>Written by {blog.author}</p>     
                 </div>
             })}
         </div>
