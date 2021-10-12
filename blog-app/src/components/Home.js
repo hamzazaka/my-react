482a97bded80535996281d2d90ef60362014413f
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-12 16:41:45 -0400

    working on the bloging home component

--- blog-app/src/components/Home.js
+++ blog-app/src/components/Home.js
@@ -1,20 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Bloglist from "./bloglist";
 
 export default function Home() {
   const [blogs,setBlogs]=useState([
       {title:'My new website',body:'lorem ipsum ......',author:'mario',id:1},
       {title:'Welcome Party',body:'lorem ipsum ......',author:'yoshi',id:2},
-      {title:'Web dev Top Tips',body:'lorem ipsum ......',author:'hamza',id:3}
+      {title:'Web dev Top Tips',body:'lorem ipsum ......',author:'mario',id:3}
   ]);
+
+  const [name,setName]=useState('mario')
+
+
+    const handleDelete=(id)=>{
+        const newBlogs=blogs.filter(blog=>blog.id!==id);
+        setBlogs(newBlogs)
+    }
+
+    useEffect(()=>{
+      console.log('use effect ran');
+      console.log(blogs);
+    },[])
 
     return (
         <div className="home">
-            {blogs.map((blog)=>{
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>     
-                </div>
-            })}
+            <Bloglist blogs={blogs} title='All Blogs!' handleDelete={handleDelete}/>
+            <button onClick={()=>setName('hamza')}>Change name</button>
+            <p>{name}</p>
+
+
         </div>
     )
 }
