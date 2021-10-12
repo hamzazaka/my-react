3fc10e0b21e2b36712c573dbb4793110e0b4b0d8
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-12 15:50:09 -0400

    working on react blog

--- blog-app/src/components/Home.js
+++ blog-app/src/components/Home.js
@@ -1,22 +1,21 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [name,setName]  =useState('hamza');
-  const [age,setAge]=useState(23);
-  let ages=[22,23,34,55,45,65];
-
-    // let name ='hamza';
-    const handleClick=()=>{
-       setName('hasnat')
-       setAge(35)
-       console.log(name);
-    }
+  const [blogs,setBlogs]=useState([
+      {title:'My new website',body:'lorem ipsum ......',author:'mario',id:1},
+      {title:'Welcome Party',body:'lorem ipsum ......',author:'yoshi',id:2},
+      {title:'Web dev Top Tips',body:'lorem ipsum ......',author:'hamza',id:3}
+  ]);
 
     return (
         <div className="home">
-            <h2>HOme page</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={()=>{handleClick()}} >Click me</button>
+            {blogs.map((blog)=>{
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+
+                </div>
+            })}
         </div>
     )
 }
