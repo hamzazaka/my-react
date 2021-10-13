1f15663fc5e2aad5c1cb8b9ed25f8e12aaa74569
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-13 02:06:22 -0400

    working on the error handling

--- blog-app/src/components/Home.js
+++ blog-app/src/components/Home.js
@@ -4,25 +4,36 @@ import Bloglist from "./bloglist";
 export default function Home() {
 
   const [blogs,setBlogs]=useState(null);
-//   const [isPending,setisPending]=useState(true);
+  const [isPending,setisPending]=useState(true);
+  const[error,setError]=useState(null);
 
 
     useEffect(()=>{
+    
       fetch('http://localhost:8000/blogs')
       .then(res=>{
-          return res.json()
+         if (!res.ok) {
+             throw Error('helo this is wrong data')
+         }
+         return res.json()
       })
       .then((data)=>{
           console.log(data);
           setBlogs(data);
-        //   setisPending(false)
+          setisPending(false);
+          setError(null);
+      })
+      .catch((err)=>{
+        setisPending(false);
+        setError(err.message);
       })
     },[])
 
 
     return (
         <div className="home">
-            {/* {isPending && <div> Loading ...</div>} */}
+            {error && <div>{error} </div>}
+            {isPending && <div> Loading ...</div>}
            {blogs && <Bloglist blogs={blogs} title='All Blogs!'/>
             }
 
