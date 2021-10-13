22c6c32099b8f9131b382a2983b7ffaa5f672cda
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-13 11:09:22 -0400

    working on the add blog page

--- blog-app/src/components/create.js
+++ blog-app/src/components/create.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Create() {
+
+    const [title,setTitle]=useState('')
+    const [body,setbody]=useState('')
+    const [author,setAuthor]=useState('mario')
+
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const blog={title,body,author};
+        console.log(blog);
+    }
+
     return (
         <div className='create'>
-            <h2>Ne blog goes here</h2>
+            <h2>New blog goes here</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="">Blog Title: </label>
+                <input type="text"
+                 required
+                 value={title}
+                 onChange={(e)=>setTitle(e.target.value)}
+                />
+                <label >Blog Body: </label>
+                <textarea required value={body} onChange={(e)=>setbody(e.target.value)} > </textarea>
+                <label >Blog Author: </label>
+                <select value={author}
+                onChange={(e)=>setAuthor(e.target.value)}> 
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+            </form>
         </div>
     )
 }
